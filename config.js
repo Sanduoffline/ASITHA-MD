074dc7fbb4984acd358e0602d120f1429fd44ee7
Sanduoffline <Induwaramadushan999@gmail.com>
2024-10-08 00:52:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3yp0RSAS#DI5gzIq7dOHGvHopSRtqaMRHJrmUu6ORErFF2kJCPmM",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
